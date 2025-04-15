32ce205d78f0887834832abb02248f2223f058fa
Balint817 <fake@email.com>
2025-04-15 10:35:34 +0200

    untested google

--- CustomerApp/View/GoogleLogin.xaml.cs
+++ CustomerApp/View/GoogleLogin.xaml.cs
@@ -7,10 +7,28 @@ public partial class GoogleLogin : ContentPage
 {
     private static readonly Regex RedirectRegex = RedirectRegexCompiled();
 
+    private const string FakeUserAgent = "Mozilla/5.0 (Linux; Android 4.1.1; Galaxy Nexus Build/JRO03C) AppleWebKit/535.19 (KHTML, like Gecko) Chrome/18.0.1025.166 Mobile Safari/535.19";
     public GoogleLogin()
     {
-        //InitializeComponent();
-        //LoginWebView.Source = "https://mateszadam.koyeb.app/user/google";
+        InitializeComponent();
+        ClearWebViewData();
+        LoginWebView.UserAgent = "fastFoodAuth";
+        LoginWebView.Source = "https://mateszadam.koyeb.app/user/google";
+    }
+    private void ClearWebViewData()
+    {
+#if ANDROID
+        var webView = new Android.Webkit.WebView(Android.App.Application.Context);
+        Android.Webkit.CookieManager.Instance.RemoveAllCookies(null);
+        Android.Webkit.CookieManager.Instance.Flush();
+        webView.ClearCache(true);
+        webView.ClearFormData();
+        webView.ClearHistory();
+#elif IOS
+        var websiteDataTypes = new Foundation.NSSet<Foundation.NSString>(WebKit.WKWebsiteDataStore.AllWebsiteDataTypes);
+        var dateFrom = Foundation.NSDate.DistantPast;
+        WebKit.WKWebsiteDataStore.DefaultDataStore.RemoveData(websiteDataTypes, dateFrom, () => { });
+#endif
     }
 
     private async void LoginWebView_Navigating(object sender, WebNavigatingEventArgs e)
@@ -22,6 +40,8 @@ public partial class GoogleLogin : ContentPage
             e.Cancel = true;
 
             string token = match.Groups[1].Value;
+            Console.WriteLine(e.Url);
+            Console.WriteLine(token);
 
             var result = await AuthService.Instance.GoogleLogin(token);
 
@@ -38,6 +58,6 @@ public partial class GoogleLogin : ContentPage
         }
     }
 
-    [GeneratedRegex(@"^https:\/\/mateszadam\.koyeb\.app\/([\w\-]+)$", RegexOptions.Compiled)]
+    [GeneratedRegex(@"^https:\/\/mateszadam\.koyeb\.app\/([A-Za-z0-9\-]+)$", RegexOptions.Compiled)]
     private static partial Regex RedirectRegexCompiled();
 }
